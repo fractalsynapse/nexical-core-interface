f4bb3a0b40d66dfe1dc646094c77538e7de52474
Adrian Webb <adrian@webb.sh>
2024-05-25 19:34:42 -0400

    Readding debug toolbar in the local settings.

--- config/settings/local/ui.py
+++ config/settings/local/ui.py
@@ -19,9 +19,9 @@ INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
 DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    # "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
-    "SHOW_TOOLBAR_CALLBACK": lambda r: False,
+    # "SHOW_TOOLBAR_CALLBACK": lambda r: False,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
