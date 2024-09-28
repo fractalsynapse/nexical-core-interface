52d150a2eb70241c12ce5d4d377c1f18ca507eef
Adrian Webb <adrian@webb.sh>
2024-09-28 01:37:51 -0400

    Updating local configurations to work with Kubernetes deployment.

--- config/settings/local/ui.py
+++ config/settings/local/ui.py
@@ -1,5 +1,4 @@
 from .base import *  # noqa
-from .base import env
 
 # URLS
 # ------------------------------------------------------------------------------
@@ -9,7 +8,7 @@ ROOT_URLCONF = "config.ui_urls"
 # WhiteNoise
 # ------------------------------------------------------------------------------
 # http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
-INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa: F405
+INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa: F405,E501
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
@@ -21,23 +20,16 @@ MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 DEBUG_TOOLBAR_CONFIG = {
     # "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
-    # "SHOW_TOOLBAR_CALLBACK": lambda r: False,
+    "SHOW_TOOLBAR_CALLBACK": lambda r: True,
 }
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
-    import socket
-
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
-    try:
-        _, _, ips = socket.gethostbyname_ex("node")
-        INTERNAL_IPS.extend(ips)
-    except socket.gaierror:
-        # The node container isn't started (yet?)
-        pass
 
 # MIDDLEWARE
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-MIDDLEWARE += ["app.global.middleware.user_timezone.TimezoneMiddleware"]  # noqa: F405
+MIDDLEWARE += ["app.global.middleware.user_timezone.TimezoneMiddleware"]  # noqa: E501
+
+# SECURITY
+# ------------------------------------------------------------------------------
+# django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
